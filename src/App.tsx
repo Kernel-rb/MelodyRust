40e4b85a5a727f6999e9e981143a7f20b3f06e0a
Kernel-rb <matabsaifeddine@gmail.com>
2024-01-13 13:44:40 +0100

    UPDATE THE ICONS & UPDATE FRONT END

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
-import { useState } from "react";
-import { invoke } from "@tauri-apps/api/tauri";
-import Footer from "./Component/footer/Footer";
-import "./App.css";
+import React from 'react';
+import Footer from './Component/footer/Footer';
+import './App.css';
+
 
 function App() {
   return (
     <div className="container">
       <div className="main-content">
-        <h1>Welcome to MelodyRust 👋</h1>
+        <h1>
+          <span>W</span><span>e</span><span>l</span><span>c</span><span>o</span><span>m</span><span>e</span>
+          {' '}<span>t</span><span>o</span>{' '}
+          <span>M</span><span>e</span><span>l</span><span>o</span><span>d</span><span>y</span><span>R</span><span>u</span><span>s</span><span>t</span> 👋
+        </h1>
         <p>Where Code Meets Rhythm</p>
-  
+        <button>I'm Ready</button>
+        <button>Don't click Me</button>
       </div>
-
       <Footer author="Kernel-rb" />
     </div>
   );
--- src/Component/footer/Footer.tsx
+++ src/Component/footer/Footer.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 
+import './Footer.css';
+
 interface FooterProps {
   author: string;
 }
 
 const Footer: React.FC<FooterProps> = ({ author }) => {
-
-
+  
   return (
-    <footer>
-      Made with ❤️ by {author}. Feel free to contribute to the <a href="https://github.com/Kernel-rb" style={{ color: 'cyan' }}> project</a>.
+    <footer style={{color : "#1BFD9C"}}>
+      Made with ❤️ by {author}. Feel free to contribute to the <a 
+      href="https://github.com/Kernel-rb" 
+      target="_blank"
+      style={{ color: "white" }}
+      > 
+      project
+      </a>.
+
     </footer>
   );
 };
